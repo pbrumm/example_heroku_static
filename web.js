b9ea0d27755b6e4a6230d12ad06fcc3936e794ad
Pete Brumm <pete@petebrumm.com>
2013-02-10 14:31:17 -0600

    adding support for static files

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+app.configure(function(){
+ app.use(express.static(__dirname + '/public')); 
+})
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
